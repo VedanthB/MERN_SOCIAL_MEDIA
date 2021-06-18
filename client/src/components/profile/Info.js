73cda989117e3504a38cf8185d3c973bed234fad
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-06-18 10:03:30 +0530

    Follow/Unfollow

--- client/src/components/profile/Info.js
+++ client/src/components/profile/Info.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Avatar from "../Avatar";
 import EditProfile from "./EditProfile";
-// import FollowBtn from "../FollowBtn";
-// import Followers from "./Followers";
-// import Following from "./Following";
+import FollowBtn from "../FollowBtn";
+import Followers from "./Followers";
+import Following from "./Following";
 import { GLOBALTYPES } from "../../redux/actions/globalTypes";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -16,8 +16,8 @@ const Info = () => {
   const [userData, setUserData] = useState([]);
   const [onEdit, setOnEdit] = useState(false);
 
-  //   const [showFollowers, setShowFollowers] = useState(false);
-  //   const [showFollowing, setShowFollowing] = useState(false);
+  const [showFollowers, setShowFollowers] = useState(false);
+  const [showFollowing, setShowFollowing] = useState(false);
 
   useEffect(() => {
     if (id === auth.user?._id) {
@@ -29,13 +29,13 @@ const Info = () => {
     }
   }, [id, auth, dispatch, profile.users]);
 
-  //   useEffect(() => {
-  //     if (showFollowers || showFollowing || onEdit) {
-  //       dispatch({ type: GLOBALTYPES.MODAL, payload: true });
-  //     } else {
-  //       dispatch({ type: GLOBALTYPES.MODAL, payload: false });
-  //     }
-  //   }, [showFollowers, showFollowing, onEdit, dispatch]);
+  useEffect(() => {
+    if (showFollowers || showFollowing || onEdit) {
+      dispatch({ type: GLOBALTYPES.MODAL, payload: true });
+    } else {
+      dispatch({ type: GLOBALTYPES.MODAL, payload: false });
+    }
+  }, [showFollowers, showFollowing, onEdit, dispatch]);
 
   return (
     <div className="info">
@@ -46,46 +46,52 @@ const Info = () => {
           <div className="info_content">
             <div className="info_content_title">
               <h2>{user.username}</h2>
-
-              <button
-                className="btn btn-outline-info"
-                onClick={() => setOnEdit(true)}
-              >
-                Edit Profile
-              </button>
+              {user._id === auth.user._id ? (
+                <button
+                  className="btn btn-outline-info"
+                  onClick={() => setOnEdit(true)}
+                >
+                  Edit Profile
+                </button>
+              ) : (
+                <FollowBtn user={user} />
+              )}
             </div>
 
             <div className="follow_btn">
-              <span className="mr-4">{user?.followers.length} Followers</span>
-              <span className="ml-4">{user?.following.length} Following</span>
+              <span className="mr-4" onClick={() => setShowFollowers(true)}>
+                {user.followers.length} Followers
+              </span>
+              <span className="ml-4" onClick={() => setShowFollowing(true)}>
+                {user.following.length} Following
+              </span>
             </div>
 
             <h6>
-              {user?.fullname}{" "}
-              <span className="text-danger">{user?.mobile}</span>
+              {user.fullname} <span className="text-danger">{user.mobile}</span>
             </h6>
-            <p className="m-0">{user?.address}</p>
-            <h6 className="m-0">{user?.email}</h6>
-            <a href={user?.website} target="_blank" rel="noreferrer">
-              {user?.website}
+            <p className="m-0">{user.address}</p>
+            <h6 className="m-0">{user.email}</h6>
+            <a href={user.website} target="_blank" rel="noreferrer">
+              {user.website}
             </a>
             <p>{user.story}</p>
           </div>
 
           {onEdit && <EditProfile setOnEdit={setOnEdit} />}
 
-          {/* {showFollowers && (
+          {showFollowers && (
             <Followers
               users={user.followers}
               setShowFollowers={setShowFollowers}
             />
-          )} */}
-          {/* {showFollowing && (
+          )}
+          {showFollowing && (
             <Following
               users={user.following}
               setShowFollowing={setShowFollowing}
             />
-          )} */}
+          )}
         </div>
       ))}
     </div>
--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -50,94 +50,94 @@ const userCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
-  //   follow: async (req, res) => {
-  //     try {
-  //       const user = await Users.find({
-  //         _id: req.params.id,
-  //         followers: req.user._id,
-  //       });
-  //       if (user.length > 0)
-  //         return res.status(500).json({ msg: "You followed this user." });
+  follow: async (req, res) => {
+    try {
+      const user = await Users.find({
+        _id: req.params.id,
+        followers: req.user._id,
+      });
+      if (user.length > 0)
+        return res.status(500).json({ msg: "You followed this user." });
 
-  //       const newUser = await Users.findOneAndUpdate(
-  //         { _id: req.params.id },
-  //         {
-  //           $push: { followers: req.user._id },
+      const newUser = await Users.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { followers: req.user._id },
+        },
+        { new: true }
+      ).populate("followers following", "-password");
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: { following: req.params.id },
+        },
+        { new: true }
+      );
+
+      res.json({ newUser });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  unfollow: async (req, res) => {
+    try {
+      const newUser = await Users.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $pull: { followers: req.user._id },
+        },
+        { new: true }
+      ).populate("followers following", "-password");
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $pull: { following: req.params.id },
+        },
+        { new: true }
+      );
+
+      res.json({ newUser });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  // suggestionsUser: async (req, res) => {
+  //   try {
+  //     const newArr = [...req.user.following, req.user._id];
+
+  //     const num = req.query.num || 10;
+
+  //     const users = await Users.aggregate([
+  //       { $match: { _id: { $nin: newArr } } },
+  //       { $sample: { size: Number(num) } },
+  //       {
+  //         $lookup: {
+  //           from: "users",
+  //           localField: "followers",
+  //           foreignField: "_id",
+  //           as: "followers",
   //         },
-  //         { new: true }
-  //       ).populate("followers following", "-password");
-
-  //       await Users.findOneAndUpdate(
-  //         { _id: req.user._id },
-  //         {
-  //           $push: { following: req.params.id },
+  //       },
+  //       {
+  //         $lookup: {
+  //           from: "users",
+  //           localField: "following",
+  //           foreignField: "_id",
+  //           as: "following",
   //         },
-  //         { new: true }
-  //       );
+  //       },
+  //     ]).project("-password");
 
-  //       res.json({ newUser });
-  //     } catch (err) {
-  //       return res.status(500).json({ msg: err.message });
-  //     }
-  //   },
-  //   unfollow: async (req, res) => {
-  //     try {
-  //       const newUser = await Users.findOneAndUpdate(
-  //         { _id: req.params.id },
-  //         {
-  //           $pull: { followers: req.user._id },
-  //         },
-  //         { new: true }
-  //       ).populate("followers following", "-password");
-
-  //       await Users.findOneAndUpdate(
-  //         { _id: req.user._id },
-  //         {
-  //           $pull: { following: req.params.id },
-  //         },
-  //         { new: true }
-  //       );
-
-  //       res.json({ newUser });
-  //     } catch (err) {
-  //       return res.status(500).json({ msg: err.message });
-  //     }
-  //   },
-  //   suggestionsUser: async (req, res) => {
-  //     try {
-  //       const newArr = [...req.user.following, req.user._id];
-
-  //       const num = req.query.num || 10;
-
-  //       const users = await Users.aggregate([
-  //         { $match: { _id: { $nin: newArr } } },
-  //         { $sample: { size: Number(num) } },
-  //         {
-  //           $lookup: {
-  //             from: "users",
-  //             localField: "followers",
-  //             foreignField: "_id",
-  //             as: "followers",
-  //           },
-  //         },
-  //         {
-  //           $lookup: {
-  //             from: "users",
-  //             localField: "following",
-  //             foreignField: "_id",
-  //             as: "following",
-  //           },
-  //         },
-  //       ]).project("-password");
-
-  //       return res.json({
-  //         users,
-  //         result: users.length,
-  //       });
-  //     } catch (err) {
-  //       return res.status(500).json({ msg: err.message });
-  //     }
-  //   },
+  //     return res.json({
+  //       users,
+  //       result: users.length,
+  //     });
+  //   } catch (err) {
+  //     return res.status(500).json({ msg: err.message });
+  //   }
+  // },
 };
 
 module.exports = userCtrl;
