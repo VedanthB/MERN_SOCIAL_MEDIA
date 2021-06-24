efba01d39127cf00289742363bc99ac4a4879e0c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-06-24 13:03:55 +0530

    images and stream

--- client/src/components/statusModal.js
+++ client/src/components/statusModal.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { GLOBALTYPES } from "../redux/actions/globalTypes";
 // import { createPost, updatePost } from "../redux/actions/postAction";
 import Icons from "./Icons";
-// import { imageShow, videoShow } from "../utils/mediaShow";
+import { imageShow, videoShow } from "../utils/mediaShow";
 
 const StatusModal = () => {
   const { auth, theme, status, socket } = useSelector((state) => state);
@@ -17,91 +17,91 @@ const StatusModal = () => {
   const refCanvas = useRef();
   const [tracks, setTracks] = useState("");
 
-  //   const handleChangeImages = (e) => {
-  //     const files = [...e.target.files];
-  //     let err = "";
-  //     let newImages = [];
+    const handleChangeImages = (e) => {
+      const files = [...e.target.files];
+      let err = "";
+      let newImages = [];
 
-  //     files.forEach((file) => {
-  //       if (!file) return (err = "File does not exist.");
+      files.forEach((file) => {
+        if (!file) return (err = "File does not exist.");
 
-  //       if (file.size > 1024 * 1024 * 5) {
-  //         return (err = "The image/video largest is 5mb.");
-  //       }
+        if (file.size > 1024 * 1024 * 5) {
+          return (err = "The image/video largest is 5mb.");
+        }
 
-  //       return newImages.push(file);
-  //     });
+        return newImages.push(file);
+      });
 
-  //     if (err) dispatch({ type: GLOBALTYPES.ALERT, payload: { error: err } });
-  //     setImages([...images, ...newImages]);
-  //   };
+      if (err) dispatch({ type: GLOBALTYPES.ALERT, payload: { error: err } });
+      setImages([...images, ...newImages]);
+    };
 
-  //   const deleteImages = (index) => {
-  //     const newArr = [...images];
-  //     newArr.splice(index, 1);
-  //     setImages(newArr);
-  //   };
+    const deleteImages = (index) => {
+      const newArr = [...images];
+      newArr.splice(index, 1);
+      setImages(newArr);
+    };
 
-  //   const handleStream = () => {
-  //     setStream(true);
-  //     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  //       navigator.mediaDevices
-  //         .getUserMedia({ video: true })
-  //         .then((mediaStream) => {
-  //           videoRef.current.srcObject = mediaStream;
-  //           videoRef.current.play();
+    const handleStream = () => {
+      setStream(true);
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then((mediaStream) => {
+            videoRef.current.srcObject = mediaStream;
+            videoRef.current.play();
 
-  //           const track = mediaStream.getTracks();
-  //           setTracks(track[0]);
-  //         })
-  //         .catch((err) => console.log(err));
-  //     }
-  //   };
+            const track = mediaStream.getTracks();
+            setTracks(track[0]);
+          })
+          .catch((err) => console.log(err));
+      }
+    };
 
-  //   const handleCapture = () => {
-  //     const width = videoRef.current.clientWidth;
-  //     const height = videoRef.current.clientHeight;
+    const handleCapture = () => {
+      const width = videoRef.current.clientWidth;
+      const height = videoRef.current.clientHeight;
 
-  //     refCanvas.current.setAttribute("width", width);
-  //     refCanvas.current.setAttribute("height", height);
+      refCanvas.current.setAttribute("width", width);
+      refCanvas.current.setAttribute("height", height);
 
-  //     const ctx = refCanvas.current.getContext("2d");
-  //     ctx.drawImage(videoRef.current, 0, 0, width, height);
-  //     let URL = refCanvas.current.toDataURL();
-  //     setImages([...images, { camera: URL }]);
-  //   };
+      const ctx = refCanvas.current.getContext("2d");
+      ctx.drawImage(videoRef.current, 0, 0, width, height);
+      let URL = refCanvas.current.toDataURL();
+      setImages([...images, { camera: URL }]);
+    };
 
-  //   const handleStopStream = () => {
-  //     tracks.stop();
-  //     setStream(false);
-  //   };
+    const handleStopStream = () => {
+      tracks.stop();
+      setStream(false);
+    };
 
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     if (images.length === 0)
-  //       return dispatch({
-  //         type: GLOBALTYPES.ALERT,
-  //         payload: { error: "Please add your photo." },
-  //       });
+    // const handleSubmit = (e) => {
+    //   e.preventDefault();
+    //   if (images.length === 0)
+    //     return dispatch({
+    //       type: GLOBALTYPES.ALERT,
+    //       payload: { error: "Please add your photo." },
+    //     });
 
-  //     if (status.onEdit) {
-  //       dispatch(updatePost({ content, images, auth, status }));
-  //     } else {
-  //       dispatch(createPost({ content, images, auth, socket }));
-  //     }
+    //   if (status.onEdit) {
+    //     dispatch(updatePost({ content, images, auth, status }));
+    //   } else {
+    //     dispatch(createPost({ content, images, auth, socket }));
+    //   }
 
-  //     setContent("");
-  //     setImages([]);
-  //     if (tracks) tracks.stop();
-  //     dispatch({ type: GLOBALTYPES.STATUS, payload: false });
-  //   };
+    //   setContent("");
+    //   setImages([]);
+    //   if (tracks) tracks.stop();
+    //   dispatch({ type: GLOBALTYPES.STATUS, payload: false });
+    // };
 
-  //   useEffect(() => {
-  //     if (status.onEdit) {
-  //       setContent(status.content);
-  //       setImages(status.images);
-  //     }
-  //   }, [status]);
+    useEffect(() => {
+      if (status.onEdit) {
+        setContent(status.content);
+        setImages(status.images);
+      }
+    }, [status]);
 
   return (
     <div className="status_modal">
@@ -137,7 +137,7 @@ const StatusModal = () => {
             <div className="flex-fill"></div>
             <Icons setContent={setContent} content={content} theme={theme} />
           </div>
-          {/* 
+          
           <div className="show_images">
             {images.map((img, index) => (
               <div key={index} id="file_img">
@@ -197,7 +197,7 @@ const StatusModal = () => {
                 </div>
               </>
             )}
-          </div> */}
+          </div>
         </div>
 
         <div className="status_footer">
