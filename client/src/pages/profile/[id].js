1380add3a3da7c472e062d419527fb45ccb6473b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-06-28 21:29:03 +0530

    display saved on profile page

--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import LoadIcon from "../../images/loading.gif";
+import React, { useEffect, useState } from "react";
+
 import Info from "../../components/profile/Info";
+import Posts from "../../components/profile/Posts";
+import Saved from "../../components/profile/Saved";
+
+import { useSelector, useDispatch } from "react-redux";
+import LoadIcon from "../../images/loading.gif";
 import { getProfileUsers } from "../../redux/actions/profileAction";
 import { useParams } from "react-router-dom";
-import Posts from "../../components/profile/Posts";
 
-function Profile() {
+const Profile = () => {
   const { profile, auth } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const { id } = useParams();
+  const [saveTab, setSaveTab] = useState(false);
 
   useEffect(() => {
     if (profile.ids.every((item) => item !== id)) {
@@ -20,15 +24,38 @@ function Profile() {
 
   return (
     <div className="profile">
-      {profile.loading ? (
-        <img className="d-block mx-auto my-4" src={LoadIcon} alt="loading" />
-      ) : (
-        <Info auth={auth} profile={profile} dispatch={dispatch} id={id} />
+      <Info auth={auth} profile={profile} dispatch={dispatch} id={id} />
+
+      {auth.user._id === id && (
+        <div className="profile_tab">
+          <button
+            className={saveTab ? "" : "active"}
+            onClick={() => setSaveTab(false)}
+          >
+            Posts
+          </button>
+          <button
+            className={saveTab ? "active" : ""}
+            onClick={() => setSaveTab(true)}
+          >
+            Saved
+          </button>
+        </div>
       )}
 
-      <Posts auth={auth} profile={profile} dispatch={dispatch} id={id} />
+      {profile.loading ? (
+        <img className="d-block mx-auto" src={LoadIcon} alt="loading" />
+      ) : (
+        <>
+          {saveTab ? (
+            <Saved auth={auth} dispatch={dispatch} />
+          ) : (
+            <Posts auth={auth} profile={profile} dispatch={dispatch} id={id} />
+          )}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default Profile;
