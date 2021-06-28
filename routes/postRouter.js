8ae0b18211533972a5cbb8c0a9da88c808978d41
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-06-28 17:24:09 +0530

    get user post ctrl / router

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -15,7 +15,7 @@ router.patch("/post/:id/like", auth, postCtrl.likePost);
 
 router.patch("/post/:id/unlike", auth, postCtrl.unLikePost);
 
-// router.get("/user_posts/:id", auth, postCtrl.getUserPosts);
+router.get("/user_posts/:id", auth, postCtrl.getUserPosts);
 
 // router.get("/post_discover", auth, postCtrl.getPostsDicover);
 
